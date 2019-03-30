577c7b96b22331dea9820ddaaa6a98576c625756
Pdbz199 <279pdbz199@gmail.com>
2019-03-30 09:31:43 -0700

    Added nice transitions for hover on navbar

---
+++ js/index.js
@@ -0,0 +1,12 @@
+d3.selectAll("a").each(function() {
+    let link = d3.select(this)
+    link.style("background", "#005ce6")
+
+    link.on("mouseenter", function() {
+        link.transition().duration(300)
+            .style("background", "#0047b3")
+    }).on("mouseout", function() {
+        link.transition().duration(300)
+            .style("background", "#005ce6")
+    })
+})
